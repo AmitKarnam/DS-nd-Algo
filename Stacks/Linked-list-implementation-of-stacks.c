3d4c6587c58f7f98a8ac8bcdd9aac304a1084556
AmitKarnam <56480746+AmitKarnam@users.noreply.github.com>
2021-01-20 17:52:34 +0530

    Update Linked-list-implementation-of-stacks.c

--- Stacks/Linked-list-implementation-of-stacks.c
+++ Stacks/Linked-list-implementation-of-stacks.c
@@ -1,58 +1,103 @@
 #include<stdio.h>
 #include<stdlib.h>
-void push(int x);
-void pop();
-void print();
 
-struct node
-{
+struct node{
     int data;
-    struct node *link;
+    struct node* next;
 };
-struct node *top=NULL;
 
-void push(int x)
-{
-    struct node *newnode;
-    newnode=(struct node*)malloc(sizeof(struct node));
-    newnode->data=x;
-    newnode->link=top;
-    top=newnode;
-}
+struct node* newnode;
+struct node* head = NULL;
+struct node* top = NULL;
 
-void pop()
+void Push(int element)
 {
-    struct node *temp;
-    if(top==NULL)
+    newnode = malloc(sizeof(struct node));
+    newnode->data = element;
+    newnode->next = NULL;
+    
+    if(head != NULL & top != NULL)
     {
-        printf("ERROR! Stack empty");
-        return;
-    }
-    temp=top;
-    top=top->link;
-    free(temp);
-}
-
-void print()
-{
-    struct node *temp;
-    temp=top;
-    printf("STACK:\n");
-    while(temp!=NULL)
-    {
-    printf("Stack data: %d\n",temp->data);
-    printf("Next stack address:%d\n",temp->link);
-    temp=temp->link;
+        top->next = newnode;
+        top = newnode;
     }
     
+    else
+    {
+        head = newnode;
+        top = newnode;
+    }
+}
+
+void Pop()
+{
+    struct node* temp = head;
+    while(temp->next != top)
+        temp = temp->next;
+        
+    temp->next = NULL;
+    top = temp;
+}
+
+void print_linked_list_stack()
+{
+    struct node *temp = head;
+     int i=1;
+     printf("STACK : \n");
+    while(temp != NULL)
+    {
+        printf("Stack Node %d info\n",i);
+        printf("Stack Node %d data:%d\n",i,temp->data);
+        printf("Address of next node is:%p\n",temp->next);
+        temp=temp->next;
+        i++;
+        printf("\n");
+    }
+
 }
 
 void main()
 {
-    push(2);
-    push(4);
-    pop();
-    push(10);
+    /* Sample Input */
     
-    print();
+    Push(1);
+    Push(2);
+    Push(3);
+    print_linked_list_stack();
+    Pop();
+    printf("After pop operation \n");
+    print_linked_list_stack();
 }
+
+
+/* Sample Output */
+
+/* 
+
+Starting program: /home/a.out                                                                
+STACK :                                                                                      
+Stack Node 1 info                                                                            
+Stack Node 1 data:1                                                                          
+Address of next node is:0x602030                                                             
+                                                                                             
+Stack Node 2 info                                                                            
+Stack Node 2 data:2                                                                          
+Address of next node is:0x602050                                                             
+                                                                                             
+Stack Node 3 info                                                                            
+Stack Node 3 data:3                                                                          
+Address of next node is:(nil)                                                                
+                                                                                             
+After pop operation                                                                          
+STACK :                                                                                      
+Stack Node 1 info                                                                            
+Stack Node 1 data:1                                                                          
+Address of next node is:0x602030                                                             
+                                                                                             
+Stack Node 2 info                                                                            
+Stack Node 2 data:2                                                                          
+Address of next node is:(nil) 
+
+*/
+
+
