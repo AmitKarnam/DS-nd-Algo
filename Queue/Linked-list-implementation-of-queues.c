2bae12080032e5713575460cf03bb0aa060f99ea
AmitKarnam <56480746+AmitKarnam@users.noreply.github.com>
2021-01-20 17:50:35 +0530

    Update Linked-list-implementation-of-queues.c

--- Queue/Linked-list-implementation-of-queues.c
+++ Queue/Linked-list-implementation-of-queues.c
@@ -1,95 +1,135 @@
-
 #include<stdio.h>
 #include<stdlib.h>
 
-/* Queue is a first in first out(fifo) data structure */
-/* Queue is a List with the restriction that insertion can be performed at rear end */
-/* enqueue or psuh is one of the operation performed at rear end */
+
+/* Queue is a first in first out(fifo) data structure */ 
+/* Queue is a List with the restriction that insertion can be performed at rear end */ 
+/* enqueue or psuh is one of the operation performed at rear end */ 
 /* dequeue or pop is one of the operation performed at front end*/
 
-void  enqueue(float data);
-void dequeue();
-void print_queue();
+struct node
+{
+    int data;
+    struct node* next;
+};
 
-struct queue
+struct node *front = NULL;
+struct node *rear = NULL;
+struct node* newnode;
+
+void Enqueue(int element)
+{
+    newnode = malloc(sizeof(struct node));
+    newnode->data = element;
+    newnode->next = NULL;
+    
+    if(front != NULL && rear != NULL)
     {
-        float data;
-        struct queue *link;      
-    };
+        rear->next = newnode;
+        rear = newnode;
+    }
+    
+    else
+    {
+        front = newnode;
+        rear = newnode;
+    }
+}
 
-struct queue *head=0,*rear,*newnode;
+void Dequeue()
+{
+    if(front->next != NULL)
+        front = front->next;
+        
+    else
+    {
+        front = NULL;
+        rear = NULL;
+    }
+}
+
+void print_linked_list_queue()
+{
+    struct node *temp = front;
+     int i=1;
+     printf("QUEUE : \n");
+    while(temp != NULL)
+    {
+        printf("Queue Node %d info\n",i);
+        printf("Queue Node %d data:%d\n",i,temp->data);
+        printf("Address of next node is:%p\n",temp->next);
+        temp=temp->next;
+        i++;
+        printf("\n");
+    }
+
+}
 
 void main()
 {
-    enqueue(1);
-    enqueue(2);
-    enqueue(3);
-    dequeue();
-    enqueue(4);
-    print_queue();
-
-}
-
-void  enqueue(float data)
-{
-
-    newnode = (struct queue*)malloc(sizeof(struct queue));
-
-    newnode->data=data;
-    newnode->link=NULL;
-
-    if(head == 0)
-        {
-            head = newnode;
-            rear = head;
-        }
-
-    else
-        {
-            rear->link = newnode;
-            rear = newnode;
-        }
-
-
-}
-
-
-void dequeue()
-{
-    if(head == rear)
-        {
-            printf("Queue at its full capacity can't dequeue.");
-            exit;
-        } 
-            
-    head = head->link;      
-       
-
+    /* Sample Input */
     
+    Enqueue(1);
+    Enqueue(2);
+    Enqueue(3);
+    Enqueue(4);
+    Enqueue(5);
+    Enqueue(6);
+    print_linked_list_queue();
+    printf("\nAfter Dequeue operation : \n");
+    Dequeue();
+    print_linked_list_queue();
 }
 
+/*  Sample Output */
 
-void print_queue()
-{
-    struct queue* temp1;
-    temp1 = head;
-    int i=1;
-    printf("QUEUE INFO : \n");
-    printf("\n");
-    while(temp1 != NULL)
-    {
-        
+/* 
 
-        printf("Node %d info\n",i);
-        printf("Node %d data:%f\n",i,temp1->data);
-        printf("Address of next node is:%p\n",temp1->link);
-        printf("\n");
-        temp1 = temp1->link;
-        i++;
-    }
+QUEUE :                                                                
+Queue Node 1 info                                                      
+Queue Node 1 data:1                                                    
+Address of next node is:0xaef030                                       
+                                                                       
+Queue Node 2 info                                                      
+Queue Node 2 data:2                                                    
+Address of next node is:0xaef050                                       
+                                                                       
+Queue Node 3 info                                                      
+Queue Node 3 data:3                                                    
+Address of next node is:0xaef070                                       
+                                                                       
+Queue Node 4 info                                                      
+Queue Node 4 data:4                                                    
+Address of next node is:0xaef090                                       
+                                                                       
+Queue Node 5 info                                                      
+Queue Node 5 data:5                                                    
+Address of next node is:0xaef0b0                                       
+                                                                       
+Queue Node 6 info                                                      
+Queue Node 6 data:6                                                    
+Address of next node is:(nil) 
 
-}         
+After Dequeue operation :                                              
+QUEUE :                                                                
+Queue Node 1 info                                                      
+Queue Node 1 data:2                                                    
+Address of next node is:0xaef050                                       
+                                                                       
+Queue Node 2 info                                                      
+Queue Node 2 data:3                                                    
+Address of next node is:0xaef070                                       
+                                                                       
+Queue Node 3 info                                                      
+Queue Node 3 data:4                                                    
+Address of next node is:0xaef090                                       
+                                                                       
+Queue Node 4 info                                                      
+Queue Node 4 data:5                                                    
+Address of next node is:0xaef0b0                                       
+                                                                       
+Queue Node 5 info                                                      
+Queue Node 5 data:6                                                    
+Address of next node is:(nil)      
 
-
-
-
+*/
